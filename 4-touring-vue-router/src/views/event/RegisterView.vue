e69d658e0f54dfdea4a14da5800f2de1e3908c64
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2023-07-01 21:46:34 +0330

    4-touring-vue-router: add programmatic navigation

--- 4-touring-vue-router/src/views/event/RegisterView.vue
+++ 4-touring-vue-router/src/views/event/RegisterView.vue
@@ -1,11 +1,21 @@
 <template>
     <p>Register for the event here</p>
+    <button @click="register">Register Me</button>
 </template>
 
 
 <script>
 export default {
-props: ['event']
+    props: ['event'],
+    methods: {
+        register(){
+            // Call to API
+            //If registered then redirect to event details
+            this.$router.push({
+                name: 'event-details',
+            })
+        }
+    }
 }
 </script>
 
