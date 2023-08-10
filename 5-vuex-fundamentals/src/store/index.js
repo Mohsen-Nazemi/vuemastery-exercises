556b822a0f71cdd984e1d44fe5998079dbe02c1a
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2023-08-10 20:22:20 +0330

    5-vuex-fundamentals: fetching state

--- 5-vuex-fundamentals/src/store/index.js
+++ 5-vuex-fundamentals/src/store/index.js
@@ -1,15 +1,55 @@
 import { createStore } from 'vuex'
+import EventService from '../services/EventService.js'
 
 export default createStore({
   state: {
     user: 'Mohsen Nazemi',
-    events: []
+    events: [],
+    event: {}
   },
   mutations: {
-    ADD_EVENT(state, event){
+    ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENT(state, event){
+      state.event = event
     }
   },
-  actions: {},
+  actions: {
+
+    createEvent({ commit }, event) {
+      EventService.postEvent(event)
+        .then(() => {
+          //add event to vuex state
+          commit('ADD_EVENT', event)
+
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then(response => {
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchEvent({ commit }, id) {
+      EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
+  },
   modules: {}
 })
