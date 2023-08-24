61a45b6a9c6d27bea3a590b7f1693028aa407def
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2023-08-24 15:38:07 +0330

    6-mastering-vuex-vue2: state and getters

---
+++ 6-mastering-vuex-vue2/src/views/EventCreate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
+</template>
+
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  // computed: mapState({
+  //   userName: state => state.user.name,
+  //   userID: state => state.user.id,
+  //   categories: state => state.categories
+  // })
+
+  // simpler way to map states
+  computed:{
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])}
+}
+</script>
+
+
+<style scoped></style>
