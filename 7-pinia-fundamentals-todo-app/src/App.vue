d9fb0dbca74ac4799bdb27553c583072f36e6b56
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2023-08-27 13:50:13 +0330

    7-pinia-fundamentals-todo-app: mutating state with actions

--- 7-pinia-fundamentals-todo-app/src/App.vue
+++ 7-pinia-fundamentals-todo-app/src/App.vue
@@ -7,5 +7,11 @@ import TodoApp from './components/ToDoApp.vue'
 </template>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50
+}
 </style>
