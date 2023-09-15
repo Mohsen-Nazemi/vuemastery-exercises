df293fcc035c0f17d12198613ea9a62a618e7a85
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2023-09-15 18:53:45 +0330

    14-TypeScript-Friendly-Vue3: reactive variables with type inference

---
+++ 14-TypeScript-Friendly-Vue3/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+import fetchCount from './services/fetchCount'
+
+interface AppInfo {
+  name: string,
+  slogan: string
+}
+
+const count = ref<number | null >(null)
+
+const appInfo: AppInfo = reactive({
+  name: 'Counter',
+  slogan: 'an app you can count on'
+})
+
+onMounted(() => {
+	fetchCount((initialCount) => {
+		count.value = initialCount
+	})
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan }}</h2>
+  </div>
+  <p>{{ count }} </p>
+</template>
